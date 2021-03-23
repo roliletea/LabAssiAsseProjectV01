b815458b15ecbdf5d09a01dc543d688114395ce0
Roland Letea <roliletea11@gmail.com>
2021-03-23 14:01:28 +0200

    Andrea added two test cases for add assignment WBT

--- src/test/java/ssvv/example/AppTest.java
+++ src/test/java/ssvv/example/AppTest.java
@@ -123,9 +123,20 @@ public class AppTest
     @Test
     public void addTemaRepo(){
 
-        assertTrue(service.saveTema("5", "test", 7, 5) == 0);
+        assertEquals(0, service.saveTema("5", "test", 7, 5));
         assertEquals(fileRepository2.findOne("5").getDescriere(), "test");
 
+    }
+
+    @Test
+    public void addAssignment00(){
+        int result = service.saveTema("4", "descriere", 5, 2);
+        assertEquals(0, result);
+    }
+    @Test
+    public void addAssignment01(){
+        int result = service.saveTema("", "descriere", 5, 2);
+        assertEquals(1, result);
     }
 
     /**
